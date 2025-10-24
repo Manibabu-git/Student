dc967e5c57b41080c2619cbfa517f6cdc9d9882e
Manibabu <manibabu.uppu1@gmail.com>
2025-10-24 08:13:14 +0530

    second commit
    Fixed syntax error and added a new print statement.

--- Student.java
+++ Student.java
@@ -2,6 +2,7 @@ class Student
 {
     public static void main(String[] args)
     {
-        System.out.println("Good Morning!!!!!")
+        System.out.println("Good Morning!!!!!");
+        System.out.println("pipeline will triger when i push it "):
     }
 }
