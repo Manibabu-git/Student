5721142754518466df040111215313eb65380447
Manibabu <manibabu.uppu1@gmail.com>
2025-10-24 09:09:45 +0530

    Fix syntax error in Student.java

--- Student.java
+++ Student.java
@@ -3,7 +3,7 @@ class Student
     public static void main(String[] args)
     {
         System.out.println("Good Morning!!!!!");
-        System.out.println("pipeline will triger when i push it "):
+        System.out.println("pipeline will triger when i push it ");
         System.out.println("*******************************");
         for(int i=0;i<10;i++)
             {
