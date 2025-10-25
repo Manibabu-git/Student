bc08e2ab4784c2f791c587a2fe70e41377998685
Manibabu <manibabu.uppu1@gmail.com>
2025-10-25 11:37:14 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -3,7 +3,6 @@ class Student
     public static void main(String[] args)
     {
         System.out.println("Good Morning!!!!!");
-        System.out.println("pipeline will triger when i push it ");
         System.out.println("*******************************");
         System.out.println("numbers are : ");
         for(int i=0;i<10;i++)
